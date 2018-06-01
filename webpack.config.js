7bb2f42609d3d44a6c0b60148b19bb008fe25725
史华力 <wb-shl391697@alibaba-inc.com>
2018-06-01 18:24:26 +0800

    add dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist')
